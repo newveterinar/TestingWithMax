ecf39d2bfb5ca9a3587f9934087a0d77ec18e218
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-04-14 15:56:11 +0300

    Добавил Toast в класс "MainActivity.kt".

--- app/src/main/java/ru/geekbrains/tests/testingwithmax/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/tests/testingwithmax/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.geekbrains.tests.testingwithmax
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         btn.setOnClickListener {
             // some action
         }
+
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
     }
 
     val btn = findViewById<Button>(R.id.view_btn)
