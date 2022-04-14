b31aa1635ffeaef63baf70ea6a5f4e2795a87239
Shaparapatah <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>
2022-04-14 15:33:35 +0300

    Добавил кнопку

--- app/src/main/java/ru/geekbrains/tests/testingwithmax/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/tests/testingwithmax/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.geekbrains.tests.testingwithmax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            // some action
+        }
     }
+
+    val btn = findViewById<Button>(R.id.view_btn)
+
 }
